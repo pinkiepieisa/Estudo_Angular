241c04a92935e0ca81bc2f1259e16d46c5cfc98f
Bela <imcdsls10@gmail.com>
2026-01-19 15:18:02 -0300

    Objeto do objeto (interpolação)

--- primeiro-projeto/src/app/card/card.ts
+++ primeiro-projeto/src/app/card/card.ts
@@ -8,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class Card {
 
+  //Objeto da classe
+  planos = {
+    tipo: 'Simples',
+    preco: 100,
+    info: {
+      tipos: 'Simples mesmo',
+      precos: 2400
+    } //Objeto do objeto
+  };
+
+  /* null = undefined */
+  
+  /* Usando uma função
+  
+  tipo = 'Simples plus';
+  preco = 100;
+
+  getFullPrice() {
+    setTimeout(()=> {
+      console.log('Set Timeout');
+      this.tipo = 'Super Simples';
+    }, 4000) //É em milisegundos
+
+    return 'R$' + this.preco + ',00/Mês'
+  }*/
 }
