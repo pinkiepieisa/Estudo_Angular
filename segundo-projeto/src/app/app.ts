0efeb41d597bb086b298dc00c2758b7346d36734
Bela <imcdsls10@gmail.com>
2026-01-27 15:08:48 -0300

    Property -Event Biding

--- segundo-projeto/src/app/app.ts
+++ segundo-projeto/src/app/app.ts
@@ -40,4 +40,25 @@ export class App {
     console.log();
   }
 
+  // Event Binding
+// () está recebendo um valor da propriedade a direita
+// [] Está enviando um valor para a propriedade
+
+//Não utilizar any, buscar saber o tipo do objeto
+
+handleInputKeyup(event: KeyboardEvent){
+  const currentText = (event.target as HTMLInputElement).value;
+  //Forçando uma tipagem
+  console.log(currentText);
 }
+
+handleInputEvent(event: Event) {
+const currentText = (event.target as HTMLInputElement).value;
+//Target é a propriedade onde o elemento value está contido
+
+console.log(currentText);
+}
+
+
+}
+
