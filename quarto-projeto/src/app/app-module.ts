1fcf2da4600ad0a11501c3cc484356943a5a59b9
Bela <imcdsls10@gmail.com>
2026-01-30 16:50:59 -0300

    Novo projeto - botão completo

---
+++ quarto-projeto/src/app/app-module.ts
@@ -0,0 +1,22 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Button } from './components/button/button';
+
+@NgModule({
+  declarations: [
+    App,
+    Button
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
---
+++ quarto-projeto/src/app/components/button/button.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: false,
+  templateUrl: './button.html',
+  styleUrl: './button.scss',
+})
+export class Button {
+  @Input({ required: true, alias: 'text' }) buttonText: string = '';
+
+  @Input({ required: true, alias: 'style' }) buttonStyle: 'white' | 'purple' = 'white';
+
+  @Output('clicked') buttonClickedEmitt = new EventEmitter<void>();
+
+  onButtonClicked() {
+    this.buttonClickedEmitt.emit();
+  }
+
+  @Input({ alias: 'disabled' })
+  isDisabled: boolean = false;
+}
