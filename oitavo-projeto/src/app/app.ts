5636d698f7f65ba28325b1e416ba168be37b5694
Bela <imcdsls10@gmail.com>
2026-02-05 12:12:32 -0300

    NgStyle

--- oitavo-projeto/src/app/app.ts
+++ oitavo-projeto/src/app/app.ts
@@ -8,6 +8,46 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('oitavo-projeto');
-  fontSize: number = 200;
+  fontSize: number = 30;
   textColor: 'white' | 'pink' = 'pink';
+  buttonText: 'Branco' | 'Rosa'= 'Branco';
+  stylesString: string = 'fonte-size: 40px; color: pink';
+  stylesObj = {
+    'font-size': this.fontSize + 'px',
+    'color': this.textColor,
+  }
+
+  increaseFontSize() {
+    this.fontSize += 10;
+
+    this.stylesString = `font-size ${this.fontSize}px; color: ${this.textColor};`;
+
+    this.stylesObj = {
+    'font-size': this.fontSize + 'px',
+    'color': this.textColor,
+    }
+  }
+
+  decreaseFontSize() {
+    this.fontSize -= 10;
+
+    this.stylesString = `font-size ${this.fontSize}px; color: ${this.textColor};`;
+  }
+
+  toggleFontColor() {
+    if(this.textColor === 'white') {
+      this.textColor = 'pink';
+      this.buttonText = 'Branco';
+    } else {
+      this.textColor = 'white';
+      this.buttonText = 'Rosa';
+    }
+
+    this.stylesString = `font-size ${this.fontSize}px; color: ${this.textColor};`;
+
+    this.stylesObj = {
+    'font-size': this.fontSize + 'px',
+    'color': this.textColor,
+    }
+  }
 }
