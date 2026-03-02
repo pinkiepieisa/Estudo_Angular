049116656c6fe53a332bdf0bcf8d505ebf1eb11b
Bela <imcdsls10@gmail.com>
2026-03-02 15:52:20 -0300

    Host listener

---
+++ projeto11/src/app/directives/listener.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appListener]'
+})
+export class ListenerDirective {
+  @HostListener('click')
+  onClick(){
+    console.log('clicou!');
+  }
+
+  @HostListener('keyup', ['$event', '"Meu taclado!"'])
+    onKeyup(event: KeyboardEvent, parms2: string){
+      console.log('Digitou!', event);
+
+      console.log(parms2);
+
+      const fullText = (event.target as HTMLInputElement).value;
+
+      console.log(parms2);
+      
+    }
+
+}
