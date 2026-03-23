766373e2bb11fea1f53b9af04e054c201258d293
Bela <imcdsls10@gmail.com>
2026-03-23 12:44:30 -0300

    Diretivas com múltiplos seletores

--- directive-content-projection/src/app/components/card/directives/card-avatar-large.directive.ts
+++ directive-content-projection/src/app/components/card/directives/card-avatar-large.directive.ts
@@ -1,7 +1,7 @@
 import { Directive } from '@angular/core';
 
 @Directive({
-  selector: 'app-card-avatar-large',
+  selector: 'app-card-avatar-large, [appCardLarge]',
   host: { 'class': 'ca-u-card-avatar-large'}
 })
 export class CardAvatarLargeDirective {
--- directive-content-projection/src/app/components/card/directives/card-text-title.directive.ts
+++ directive-content-projection/src/app/components/card/directives/card-text-title.directive.ts
@@ -1,7 +1,7 @@
 import { Directive } from '@angular/core';
 
 @Directive({
-  selector: 'app-card-text-title',
+  selector: 'app-card-text-title, [appCardTextTitle]',
   // Transforma em selector de elemento
   host: { 'class': 'ca-u-card-text-title'}
 })
