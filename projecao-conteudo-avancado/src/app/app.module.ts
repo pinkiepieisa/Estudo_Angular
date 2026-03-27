64ee8bb4338394ce73e7572ce5603bca23e85086
Bela <imcdsls10@gmail.com>
2026-03-27 15:49:26 -0300

    ng-Template com ngTemplateOutlet

--- projecao-conteudo-avancado/src/app/app.module.ts
+++ projecao-conteudo-avancado/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MeuCompComponent } from './meu-comp/meu-comp.component';
+import { MeuNovoCompComponent } from './meu-novo-comp/meu-novo-comp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuCompComponent
+    MeuCompComponent,
+    MeuNovoCompComponent
   ],
   imports: [
     BrowserModule
