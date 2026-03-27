0043ee69a6bed8cbbc67bbfc89bcc994f0ddbcf3
Bela <imcdsls10@gmail.com>
2026-03-27 16:28:33 -0300

    ngIf e ngIfElse

--- projecao-conteudo-avancado/src/app/app.component.ts
+++ projecao-conteudo-avancado/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './ng-template-outlet/ex4.html',
+  templateUrl: './ng-template-outlet/ex5.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
