f7c9de7b71ba17f1bda2f4443c1a861a47f9816a
Bela <imcdsls10@gmail.com>
2026-04-09 15:22:39 -0300

    Pipe

--- pipes-project/src/app/app.module.ts
+++ pipes-project/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StatusPipe } from './pipe/status.pipe';
+import { TruncatePipe } from './pipe/truncate.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StatusPipe
+    StatusPipe,
+    TruncatePipe
   ],
   imports: [
     BrowserModule
