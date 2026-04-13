572bb4bae8e127e63727a3ceb4f20407230c4147
Bela <imcdsls10@gmail.com>
2026-04-13 10:42:39 -0300

    Pipes com endpoints

--- pipes-project/src/app/app.module.ts
+++ pipes-project/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { StatusPipe } from './pipe/status.pipe';
 import { ObsComObjetoComponent } from './components/obs-com-objeto/obs-com-objeto.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ObsComArrayComponent } from './components/obs-com-array/obs-com-array.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StatusPipe,
-    ObsComObjetoComponent
+    ObsComObjetoComponent,
+    ObsComArrayComponent
   ],
   imports: [
     BrowserModule,
--- pipes-project/src/app/services/users.service.ts
+++ pipes-project/src/app/services/users.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { IUserResponse } from "../interfaces/user-response.interface";
+import { UsersListResponse } from "../types/users-list-response.type";
 
 @Injectable({
     providedIn:'root',
@@ -12,7 +13,29 @@ export class UserService{
         private readonly _http: HttpClient
     ) {}
 
+    //Endpoints
+    getUsers(): Observable<UsersListResponse> {
+        return this._http.get<UsersListResponse>('https://jsonplaceholder.typicode.com/users');
+        //Pega todos os usuários
+    }
+
     getUserById(userId: number): Observable<IUserResponse> {
         return this._http.get<IUserResponse>(`https://jsonplaceholder.typicode.com/users/${userId}`);
+        //Pega o usuário por meio do id
+    }
+
+    // 
+
+    getUser() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({
+                    id: 1,
+                    name: "Leanne Graham",
+                    username: "Bret",
+                    email: "Sincere@april.biz"
+                });
+            }, 3000);
+        })
     }
 }
