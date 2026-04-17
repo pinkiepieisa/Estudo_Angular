05a6b729356d5b4afc540abe05c93d6b0d82a856
Bela <imcdsls10@gmail.com>
2026-04-17 13:06:07 -0300

    Input e output do ngmodel

--- TemplateDrivenForms/src/app/app.component.ts
+++ TemplateDrivenForms/src/app/app.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  nome: string = 'Bela';
+
+  onChange(text: string) {
+    console.log(text);
+
+    //...
+
+    this.nome = text; 
+    // assim o campo sempre fica atualizado
+  }
+
+  show() {
+    console.log(this.nome);
+  }
 }
