cc94a9268d8c496d08a55f892f9f00586afaf9df
Bela <imcdsls10@gmail.com>
2026-04-24 16:44:58 -0300

    ngmodelgroup

--- TemplateDrivenForms/src/app/elements/radio-button/radio-button.component.ts
+++ TemplateDrivenForms/src/app/elements/radio-button/radio-button.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-radio-button',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './radio-button.component.scss'
 })
 export class RadioButtonComponent {
+  onSubmit(meuForm: NgForm) {
+    console.log(meuForm.value);
+  }
 
 }
